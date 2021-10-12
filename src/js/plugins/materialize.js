2551bdec85ae9a12c5c9c7047b1d5f1ecc2eed2c
VladimirBeria <volodymyr.dechtiaruk@gmail.com>
2021-10-13 01:40:36 +0300

    form created

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -2,9 +2,34 @@ import 'materialize-css/dist/css/materialize.min.css'
 import 'materialize-css/dist/js/materialize.min'
 
 //init select
-const select =  document.querySelectorAll('select');
+const select = document.querySelectorAll('select');
 M.FormSelect.init(select);
 
 export function getSelectInstance(elem) {
     return M.FormSelect.getInstance(elem)
+}
+
+//init autocomplete
+const autocomplete = document.querySelectorAll('.autocomplete')
+M.Autocomplete.init(autocomplete, {
+    data: {
+        Apple: null,
+        Microsoft: null,
+        Google: 'https://placehold.it/250x250',
+    }
+})
+
+export function getAutocompleteInstance(elem) {
+    return M.Autocomplete.getInstance(elem)
+}
+
+//Date Picker init
+
+const datepicker = document.querySelectorAll('.datepicker')
+M.Datepicker.init(datepicker, {
+    showClearBtn: true
+})
+
+export function getDatePickerInstance(elem) {
+    return M.Datepicker.getInstance(elem)
 }
