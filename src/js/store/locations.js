da52a31757b3ed583215b8b5f38bd4416a422a5c
VladimirBeria <volodymyr.dechtiaruk@gmail.com>
2021-10-14 02:17:30 +0300

    cities and countries to new format for form autocomplete

--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -5,6 +5,7 @@ class Locations {
         this.api = api
         this.countries = null
         this.cities = null
+        this.shortCitiesList = null
     }
 
     async init() {
@@ -14,9 +15,39 @@ class Locations {
         ])
 
         const [countries, cities] = response;
-        this.countries = countries;
-        this.cities = cities;
+        this.countries = this.serializeCountries(countries);
+        this.cities = this.serializeCities(cities);
+        this.shortCitiesList = this.createShortCitiesList(this.cities)
         return response
+    }
+
+    createShortCitiesList(cities) {
+        return Object.entries(cities).reduce((acc, [key]) => {
+            acc[key] = null
+            return acc
+        }, {})
+    }
+
+    serializeCountries(countries) {
+        //{'Country code' : { ... }}
+        return countries.reduce((acc, country) => {
+            acc[country.code] = country
+            return acc
+        }, {})
+    }
+
+    serializeCities(cities) {
+        return cities.reduce((acc, city) => {
+            const country_name = this.getCountryNameByCode(city.country_code)
+            const city_name = city.name || city.name_translations.en
+            const key = `${city_name}, ${country_name}`
+            acc[key] = city
+            return acc
+        }, {})
+    }
+
+    getCountryNameByCode(code) {
+        return this.countries[code].name
     }
 
     getCitiesByCountryCode(code) {
