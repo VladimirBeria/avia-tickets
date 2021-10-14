1301f2d14e71d51ae80f0a4b901623e87f5fba82
VladimirBeria <volodymyr.dechtiaruk@gmail.com>
2021-10-14 15:14:32 +0300

    autocomplete cities done

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,28 @@
 import locations from "./store/locations";
 import '../css/style.css'
 import './plugins/index'
+import formUI from "./views/form";
 
-locations.init().then(res => {
-    console.log(res)
-    console.log(locations)
-    console.log(locations.getCitiesByCountryCode('PE'))
+document.addEventListener('DOMContentLoaded', () => {
+    initApp()
+    const form = formUI.form
+
+    // events
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        onFormSubmit()
+    })
+
+    //handlers
+    async function initApp() {
+        await locations.init()
+        formUI.setAutocompleteData(locations.shortCitiesList)
+    }
+    async function onFormSubmit() {
+        //get data from inputs
+        const origin = formUI.originValue
+        const destination = formUI.destinationValue
+        const depart_date = formUI.departDateValue
+        const return_date = formUI.returnDateValue
+    }
 })
-
